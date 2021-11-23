d1c84b60d90d14a54c0192b17d1a1c9093ef874b
Пилипенко Адриана Андреевна <PilAA.21@free.uni-dubna.ru>
2021-11-23 14:20:31 +0300

    pysh

---
+++ babushka.py
@@ -0,0 +1,7 @@
+
+print('Введите количество учащихся')
+n = int(input())
+for i in range (n):
+    print('Введите учащегося №', i)
+    a = {input()}
+    
---
+++ containers.py
@@ -0,0 +1,6 @@
+a = [i for i in range (1, 11)]
+a[5] = 100
+print(a)
+
+a = (1, 2, 3 ,4, 100, 6, 7, 8, 10)
+print(a)
