7cf6c7468afbc225123b2b4bad30037644187e31
Пилипенко Адриана Андреевна <PilAA.21@free.uni-dubna.ru>
2021-12-07 14:05:57 +0300

    aaaaaaaaaaaaaaaaaaaaaa

---
+++ babushka2.py
@@ -0,0 +1,72 @@
+def add_s(n):
+    count = 0
+    a = set ()
+    name = dict()
+    for i in range (n):
+        print('Введите учащегося №', i+1, 'Введите 1, чтобы остановить ввод')
+        f = True
+        try:
+            x = input()
+            if x == '1':
+                print()
+                break
+            else:
+                #забиваем список
+                set.add(a, x)
+                #вытаскиваем имя и колво баллов
+                b = x.split(' ')
+                name[b[0]] = b[2]
+                k = list(name)
+        except Exception as e:
+            print('ошибка')
+            f = False
+
+        if f == False:
+            add_s(n)
+
+    print(count)
+    print(*a, sep=', ')
+    return (a, name)
+
+#листы
+list_1 = set()
+list_2 = set()
+list_3 = set()
+ind = 0
+name = dict()
+#цикл ввода
+
+while ind < 5:
+    print('Введите 1, чтобы ввести олимпиадников по математике')
+    print('Введите 2, чтобы ввести олимпиадников по русскому')
+    print('Введите 3, чтобы ввести олимпиадников по информатике')
+    print('Введите 4, чтобы распечатать все списки')
+    print('Введите 5, чтобы закончить')
+    print('Введите 6, чтобы вывести количество баллов')
+    ind = int(input())
+
+    if ind == 1:
+        print('Введите количество учащихся:', end = ' ')
+        n = int(input())
+        list_1=add_s(n)
+    elif ind == 2:
+        print('Введите количество учащихся:', end = ' ')
+        n = int(input())
+        list_2=add_s(n)
+    elif ind == 3:
+        print('Введите количество учащихся:', end = ' ')
+        n = int(input())
+        list_3=add_s(n)
+    elif ind == 4:
+        print('Олипиада по математике: ', *list_1, *list_2, *list_3, sep='\t')
+        a = list_1.union(list_2).union(list_3)
+        print('общее количество участников: ', len(a))
+    elif ind == 5:
+        break
+    elif ind == 6:
+        word = input()
+        if word in name.keys():
+            print(name[word])
+    else:
+        print('ошибка ввода')
+
--- process.py
+++ process.py
@@ -1,11 +1,11 @@
 count = 0
 a = set ()
-name = [ ]
+name = dict()
 #Фамилия Имя 10.split()
 #[Фамилия, Имя, 10]
-
+count +=1
 #ввод: ФИО баллы  // вывод: №_И_Б
-print('Введите учащегося №', 'Введите 1, чтобы остановить ввод')
+print('Введите учащегося №', count, 'Введите 1, чтобы остановить ввод')
 try:
     x = input()
     if x == '1':
@@ -17,10 +17,9 @@ try:
         #вытаскиваем имя и колво баллов
         b = x.split(' ')
         print(b)
-        name = b[0]
-        kolvo = b[2]
+        name[b[0]] = b[2]
 
 except Exception as e:
     print('ошибка') #you died, try again?
 
-print(name[0],'_', kolvo, end = '')
+print(count, '_', name, end = '')
